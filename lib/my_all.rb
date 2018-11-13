9a257e95e8b7abd912d500ce82da3aab66234c60
Arnold Su <arnoldtingsu@gmail.com>
2018-11-13 15:40:54 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,9 +7,10 @@ def my_all?(collection)
     block_return_values <<
     yield(collection[i])
     i+=1
-    binding.pry
   end
   
+  if block_return_values.include?(false)
+    false
 end
 
 block_return_values
